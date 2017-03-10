1192e48835675fb94f1113bb4588c291a230bb8b
Peng Xu <pxu@codeaurora.org>
2017-03-09 16:16:28 -0800

    Revert "Transmit packets periodically with new option -Q"
    This reverts commit 30d6a42ba0f3083007a098731f732aa8ab97c579.
    
    CRs-fixed: 2016637
    Change-Id: I194b1a0f160a0d4e24ee7c9c3216730e5b973a90

--- sigma_dut.c
+++ sigma_dut.c
@@ -36,10 +36,6 @@ char *sigma_cert_path = "/etc/wpa_supplicant";
  * otherwise default WMM-PS 0 */
 int sigma_wmm_ac = 0;
 
-/* For VO-Enterprise testing set this to 1 through argument
- * to send periodic data.
- */
-int sigma_periodic_data = 0;
 
 #ifdef ANDROID
 #include <android/log.h>
@@ -768,7 +764,7 @@ int main(int argc, char *argv[])
 
 	for (;;) {
 		c = getopt(argc, argv,
-			   "aAb:Bc:C:dDE:e:fghH:i:Ik:l:L:m:M:nN:o:O:p:P:qQr:R:s:S:tT:uv:VWw:");
+			   "aAb:Bc:C:dDE:e:fghH:i:Ik:l:L:m:M:nN:o:O:p:P:qr:R:s:S:tT:uv:VWw:");
 		if (c < 0)
 			break;
 		switch (c) {
@@ -829,11 +825,6 @@ int main(int argc, char *argv[])
 			break;
 		case 'q':
 			sigma_dut.debug_level++;
-			break;
-		case 'Q':
-			sigma_dut_print(&sigma_dut, DUT_MSG_INFO,
-					"Send Periodic data");
-			sigma_periodic_data = 1;
 			break;
 		case 'r':
 			if (strcmp(optarg, "HT40") == 0) {
@@ -931,7 +922,7 @@ int main(int argc, char *argv[])
 			break;
 		case 'h':
 		default:
-			printf("usage: sigma_dut [-aABdfqQDIntuVW] [-p<port>] "
+			printf("usage: sigma_dut [-aABdfqDIntuVW] [-p<port>] "
 			       "[-s<sniffer>] [-m<set_maccaddr.sh>] \\\n"
 				"       [-M<main ifname>] [-R<radio ifname>] "
 			       "[-S<station ifname>] [-P<p2p_ifname>]\\\n"
